8e447a1fbcbaaa2c78e0e09ca57d6af87f9981aa
daggerbourne <61265115+daggerbourne@users.noreply.github.com>
2025-07-22 21:02:41 -0500

    upload file size limit
    changed to 100 mb

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const upload = multer({
     }
     cb(null, true);
   },
-  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+  limits: { fileSize: 100 * 1024 * 1024 } // 100MB limit
 });
 
 app.use(cors());
